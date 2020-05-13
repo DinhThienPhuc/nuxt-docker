64b1b86822630e091535a2999104963eedffa713
DinhThienPhuc <thienphuc.bka1412@gmail.com>
2020-05-13 17:36:08 +0700

    ínit nũt docker

---
+++ README.md
@@ -0,0 +1,10 @@
+# Hello World with Nuxt.js
+
+https://nuxtjs.org/examples
+
+# Docker Instruction
+
+```
+docker build -t <app-image> .
+docker run -dp <exposed-port>:<app-port> <app-image>
+```
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Welcome!</h1>
+    <NLink to="/about">Brotherhood Studio Forever</NLink>
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: "Home page"
+  }
+};
+</script>
